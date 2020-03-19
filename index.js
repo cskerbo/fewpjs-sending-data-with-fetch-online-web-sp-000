daf293341d0674e83c0d9d42bc0463ede87eb2ba
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-19 18:05:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+let submitData(name, email) = {
+  userName: name,
+  userEmail: email
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
